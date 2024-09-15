733068e5ba60de0b3b83d06782955505890daa04
Md. Fahim Bin Amin <fahimbinamin@gmail.com>
2024-09-15 16:32:47 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup
+from pathlib import Path
 
-setup()
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
